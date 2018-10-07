beb10b2a9c1255f8c077aacd520532d604dd37a4
Rozita Teymourzadeh <rteymourzadeh@dons.usfca.edu>
2018-10-07 10:54:36 -0700

    add polymorphism

--- cs601/InterfacePolymorphism/Run.java
+++ cs601/InterfacePolymorphism/Run.java
@@ -21,6 +21,21 @@ public class Run {
 		
 		Beverage e = new Coffee(false);
 		System.out.println(e.getServingGlass());
+//Polymorphism
+		Beverage[] brev = new Beverage[3];
+		brev[0] = new Coffee(false);
+		brev[1] = new Coffee(true);
+		
+//Exercise of Inheritance section		
+		System.out.println("Start...");
+		Beverage c1 = new Coffee(true);
+		Coffee c2 = new Coffee(false);
+		c1.drink();
+		c2 =(Coffee) c1;
+		c2.drink();
+		
+		
+		
 	}
 
 }
