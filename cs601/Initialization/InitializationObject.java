548c68d8681f03de20d69e9cabce12c12e697571
Rozita Teymourzadeh <rteymourzadeh@dons.usfca.edu>
2018-10-08 23:51:07 -0700

    minor

--- cs601/Initialization/InitializationObject.java
+++ cs601/Initialization/InitializationObject.java
@@ -8,8 +8,26 @@ package Initialization;
  *
  */
 public class InitializationObject {
-
+	
 	public InitializationObject(String msg) {
 		System.out.println("InitializationObject constructor " + msg);
+	}
+	
+	private String name;
+	public void setName(String msg) {
+		this.name = msg;
+	}
+	
+	public String getNeme() {
+		return name;
+	}
+
+	public boolean equals(InitializationObject anotherObj) {
+		if (anotherObj.name == null) 
+			return false;
+	    if (this.name.equals(anotherObj.name))
+	    	return true;
+	    else
+	    	return false;
 	}
 }
